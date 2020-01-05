4a4bf34400f99a112c38b4fc0aea4f0b61a1627b
Teodor Dimitrov <teodor_dimitrov98@abv.bg>
2020-01-05 16:10:22 +0200

    Project setup

---
+++ src/main/java/finalproject/ninegag/NinegagApplication.java
@@ -0,0 +1,13 @@
+package finalproject.ninegag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinegagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NinegagApplication.class, args);
+    }
+
+}
---
+++ src/test/java/finalproject/ninegag/NinegagApplicationTests.java
@@ -0,0 +1,13 @@
+package finalproject.ninegag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinegagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
