a0f6f002bf26a56b1c72639ffc0a3d73c635f486
Teodor Dimitrov <teodor_dimitrov98@abv.bg>
2020-01-08 12:42:05 +0200

    Added  pom.xml

--- src/main/java/finalproject/ninegag/model/dao/CommentDao.java
+++ src/main/java/finalproject/ninegag/model/dao/CommentDao.java
@@ -14,12 +14,78 @@ import java.sql.SQLException;
 @Component
 public class CommentDao{
 
-    public static final String SELECT_UPVOTED_COMMENT_BY_USER_ID = "SELECT FROM users_upvoted_comments WHERE user_id = ? AND comment_id = ?";
-    public static final String SELECT_DOWNVOTED_COMMENT_BY_USER_ID = "SELECT FROM users_downvoted_comments WHERE user_id = ? AND comment_id = ?";
+    public static final String SELECT_UPVOTED_COMMENT_BY_USER_ID =
+            "SELECT FROM users_upvoted_comments WHERE user_id = ? AND comment_id = ?";
+
+    public static final String SELECT_DOWNVOTED_COMMENT_BY_USER_ID =
+            "SELECT FROM users_downvoted_comments WHERE user_id = ? AND comment_id = ?";
+
+    public static final String DELETE_UPVOTED_COMMENT_BY_USER_ID =
+            "DELETE FROM users_upvoted_comments WHERE user_id = ? AND comment_id = ?";
+
+    public static final String DELETE_DOWNVOTED_COMMENT_BY_USER_ID =
+            "DELETE FROM users_downvoted_comments WHERE user_id = ? AND comment_id = ?";
+
+    public static final String INSERT_INTO_UPVOTED =
+            "INSERT INTO users_upvoted_comments (user_id, comment_id) VALUES (?, ?)";
+
+    public static final String INSERT_INTO_DOWNVOTED =
+            "INSER INTO users_downvoted_comments (user_id, comment_id) VALUES (?, ?)";
+
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+
+    public void upvoteComment(User u, Comment c) throws SQLException {
+        try(Connection connection = jdbcTemplate.getDataSource().getConnection()) {
+            if (isCommentUpvoted(u, c)) {
+                String removeFromUpvotes = DELETE_UPVOTED_COMMENT_BY_USER_ID;
+                PreparedStatement statement = connection.prepareStatement(removeFromUpvotes);
+                statement.setLong(1, u.getId());
+                statement.setLong(2, c.getId());
+                statement.executeUpdate();
+            } else {
+                if (isCommentDownvoted(u, c)) {
+                    String removeFromDownvotes = DELETE_DOWNVOTED_COMMENT_BY_USER_ID;
+                    PreparedStatement statement = connection.prepareStatement(removeFromDownvotes);
+                    statement.setLong(1, u.getId());
+                    statement.setLong(2, c.getId());
+                    statement.executeUpdate();
+                }
+                String upvote = INSERT_INTO_UPVOTED;
+                PreparedStatement statement = connection.prepareStatement(upvote);
+                    statement.setLong(1, u.getId());
+                    statement.setLong(2, c.getId());
+                    statement.executeUpdate();
+            }
+        }
+    }
+
+    public void downvoteComment(User u, Comment c) throws SQLException {
+        try(Connection connection = jdbcTemplate.getDataSource().getConnection()) {
+            if (isCommentDownvoted(u, c)) {
+                String removeFromDownvotes = DELETE_DOWNVOTED_COMMENT_BY_USER_ID ;
+                PreparedStatement statement = connection.prepareStatement(removeFromDownvotes);
+                statement.setLong(1, u.getId());
+                statement.setLong(2, c.getId());
+                statement.executeUpdate();
+            } else {
+                if (isCommentUpvoted(u, c)) {
+                    String removeFromUpvotes = DELETE_UPVOTED_COMMENT_BY_USER_ID;
+                    PreparedStatement statement = connection.prepareStatement(removeFromUpvotes);
+                    statement.setLong(1, u.getId());
+                    statement.setLong(2, c.getId());
+                    statement.executeUpdate();
+                }
+                String downvote = INSERT_INTO_DOWNVOTED;
+                PreparedStatement statement = connection.prepareStatement(downvote);
+                    statement.setLong(1, u.getId());
+                    statement.setLong(2, c.getId());
+                    statement.executeUpdate();
+            }
+        }
+    }
 
 
     private boolean isCommentUpvoted(User u, Comment c) throws SQLException {
