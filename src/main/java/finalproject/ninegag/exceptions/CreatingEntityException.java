97e202f32b155b241fcd668ba3867fb4324e529d
Abil Dermedzhiev <abil23@abv.bg>
2020-01-14 20:27:33 +0200

    Now Every post returns name of it's category and details for it's owner.Added CreatingEntityException.Validated searching post by title.

---
+++ src/main/java/finalproject/ninegag/exceptions/CreatingEntityException.java
@@ -0,0 +1,8 @@
+package finalproject.ninegag.exceptions;
+
+public class CreatingEnityException extends RuntimeException {
+
+    public CreatingEnityException(String msg) {
+        super(msg);
+    }
+}
